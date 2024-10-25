fa206c86264342250a5901944bb57ebd344f9753
Argha Dhar <dhar1707069@stud.kuet.ac.bd>
2024-10-25 11:47:44 +0600

    Update Matrix Multiplication

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-// Declaration of the assembly function
-extern void matmul_asm(int* A, int* B, int* C, int N);
+// Function to multiply two 2x2 matrices in pure C
+void matmul_c(int A[2][2], int B[2][2], int C[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 2; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
 int main() {
-    int N = 2;  // 2x2 matrices
     int A[2][2] = { {1, 2}, {3, 4} };
     int B[2][2] = { {5, 6}, {7, 8} };
     int C[2][2];  // Resultant matrix
 
-    // Call matrix multiplication using assembly
-    matmul_asm(&A[0][0], &B[0][0], &C[0][0], N);
+    // Call matrix multiplication in C
+    matmul_c(A, B, C);
 
     // Print the result
     printf("Matrix C (Result):\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
             printf("%d ", C[i][j]);
         }
         printf("\n");
