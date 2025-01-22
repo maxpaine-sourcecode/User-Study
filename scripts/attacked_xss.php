5c2e0e739ab0f285d2a82569710eebda8186707f
Argha Dhar <dhar1707069@stud.kuet.ac.bd>
2025-01-22 07:09:30 +0600

    Create attacked_xss.php

---
+++ scripts/attacked_xss.php
@@ -0,0 +1,56 @@
+<?php
+        // check login form contents
+        if (empty($_POST['user_name'])) {
+            $this->errors[] = "Username field was empty.";
+        } elseif (empty($_POST['user_password'])) {
+            $this->errors[] = "Password field was empty.";
+        } elseif (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
+
+            // create a database connection, using the constants from config/db.php (which we loaded in index.php)
+            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+            // change character set to utf8 and check it
+            if (!$this->db_connection->set_charset("utf8")) {
+                $this->errors[] = $this->db_connection->error;
+            }
+
+            // if no connection errors (= working database connection)
+            if (!$this->db_connection->connect_errno) {
+
+                // escape the POST stuff
+                $user_name = $this->db_connection->real_escape_string($_POST['user_name']);
+
+                // database query, getting all the info of the selected user (allows login via email address in the
+                // username field)
+                $sql = "SELECT user_name, user_email, user_password_hash
+                        FROM users
+                        WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
+                $result_of_login_check = $this->db_connection->query($sql);
+
+                // if this user exists
+                if ($result_of_login_check->num_rows == 1) {
+
+                    // get result row (as an object)
+                    $result_row = $result_of_login_check->fetch_object();
+
+                    // using PHP 5.5's password_verify() function to check if the provided password fits
+                    // the hash of that user's password
+                    if (password_verify($_POST['user_password'], $result_row->user_password_hash)) {
+
+                        // write user data into PHP SESSION (a file on your server)
+                        $_SESSION['user_name'] = $result_row->user_name;
+                        $_SESSION['user_email'] = $result_row->user_email;
+                        $_SESSION['user_login_status'] = 1;
+
+                    } else {
+                        $this->errors[] = "Wrong password. Try again.";
+                    }
+                } else {
+                        // Displaying wrong user name so that the user can avoid typing mistake in next attempt
+                    $this->errors[] = "This user ($user_name) does not exist.";
+                }
+            } else {
+                $this->errors[] = "Database connection problem.";
+            }
+        }
+    ?>
